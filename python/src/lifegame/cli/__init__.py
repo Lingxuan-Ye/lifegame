40cce821bd3f8d5b130260507fe1c1b920426484
Lingxuan Ye <love@lingxuan.io>
2023-11-05 06:05:18 +0800

    Rewrite command line interface

--- python/src/lifegame/cli/__init__.py
+++ python/src/lifegame/cli/__init__.py
@@ -1,46 +1,128 @@
 import click
 
+from .. import genesis, lensfilter
 from ..__about__ import __version__
-from ..biosquare import BioSquare
-from ..genesis import DicingGod, background_radiate
-from ..lensfilter import Dye, Emojify, blockify, digitize
+from ..biosquare import BioSquare, LensFilter
 from ..screen import Screen
 
 CONTEXT_SETTINGS = dict(
-    help_option_names=['-h', '--help'],
+    help_option_names=["-h", "--help"],
     default_map={
-        'nrows': 40,
-        'ncols': 40,
-        'fps_max': 24.0,
-        'cell': 'random-dye',
-        'seed': None,
-    }
+        "nrows": 32,
+        "ncols": 32,
+        "cell": "dye",
+        "color_alive": "white",
+        "color_dead": "green",
+        "fps_max": 24.0,
+        "show_stats": True,
+        "seed": None,
+    },
+)
+CellChoice = click.Choice(
+    [
+        "bit",
+        "block",
+        "emoji",
+        "dye",
+        "random-dye",
+    ],
+    case_sensitive=False,
+)
+ColorChoice = click.Choice(
+    [
+        "black",
+        "red",
+        "green",
+        "yellow",
+        "blue",
+        "magenta",
+        "cyan",
+        "white",
+    ],
+    case_sensitive=False,
 )
 
 
-@click.command(context_settings=CONTEXT_SETTINGS, epilog='Written by Lingxuan Ye <love@lingxuan.io>.')
-@click.version_option(version=__version__, prog_name='LifeGame')
-@click.option('-r', '--nrows', type=click.IntRange(min=1), show_default=True, help='Number of rows.')
-@click.option('-c', '--ncols', type=click.IntRange(min=1), show_default=True, help='Number of columns.')
-@click.option('--fps-max', type=click.FloatRange(min=0.0, min_open=True), show_default=True, help='Upper limit of fps.')
-@click.option('--cell', type=click.Choice(['binary', 'block', 'emoji', 'random-dye'], case_sensitive=False), help='Specifies cell style.')
-@click.option('--seed', type=int, help='World initialization seed.')
+@click.command(
+    context_settings=CONTEXT_SETTINGS,
+    epilog="Written by Lingxuan Ye <love@lingxuan.io>.",
+)
+@click.option(
+    "-r",
+    "--nrows",
+    metavar="",
+    type=click.IntRange(min=1),
+    help="Number of rows.",
+)
+@click.option(
+    "-c",
+    "--ncols",
+    metavar="",
+    type=click.IntRange(min=1),
+    help="Number of columns.",
+)
+@click.option(
+    "-C",
+    "--cell",
+    type=CellChoice,
+    show_default=True,
+    help="Specifies cell style.",
+)
+@click.option(
+    "-A",
+    "--color-alive",
+    type=ColorChoice,
+    help="Color for alive cells. Omit if `--cell` is not set to 'dye'.",
+)
+@click.option(
+    "-D",
+    "--color-dead",
+    type=ColorChoice,
+    help="Color for dead Cells. Omit if `--cell` is not set to 'dye'.",
+)
+@click.option(
+    "--fps-max",
+    metavar="",
+    type=click.FloatRange(min=0.0, min_open=True),
+)
+@click.option(
+    "--show-stats/--hide-stats",
+    help="Show or hide statistics.",
+)
+@click.option(
+    "--seed",
+    type=int,
+    metavar="",
+    help="Seed for world initialization.",
+)
+@click.version_option(
+    __version__,
+    "-V",
+    "--version",
+    prog_name="LifeGame",
+)
 def lifegame(
     nrows: int,
     ncols: int,
-    fps_max: int,
     cell: str,
+    color_alive: str,
+    color_dead: str,
+    fps_max: int,
+    show_stats: bool,
     seed: int,
 ):
+    filter: LensFilter
     match cell:
-        case 'binary':
-            lensfilter = digitize
-        case 'block':
-            lensfilter = blockify
-        case 'emoji':
-            lensfilter = Emojify.random()
-        case 'random-dye':
-            lensfilter = Dye.random()
-    biosquare = BioSquare(nrows, ncols, DicingGod(seed), lensfilter)
-    screen = Screen(biosquare, fps_max=fps_max)
+        case "bit":
+            filter = lensfilter.Digitize()
+        case "block":
+            filter = lensfilter.Blockify()
+        case "emoji":
+            filter = lensfilter.Emojify.random()
+        case "dye":
+            filter = lensfilter.Dye(color_alive, color_dead)
+        case "random-dye":
+            filter = lensfilter.Dye.random()
+    biosquare = BioSquare(nrows, ncols, genesis.DicingGod(seed), filter)
+    screen = Screen(biosquare, fps_max=fps_max, show_stats=show_stats)
     screen.play()
