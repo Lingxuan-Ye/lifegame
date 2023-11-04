37503ba6511f89c73f3c7417b32a363afc9b0b9a
Lingxuan Ye <love@lingxuan.io>
2023-11-05 05:45:44 +0800

    Modify re-exports

--- python/src/lifegame/__init__.py
+++ python/src/lifegame/__init__.py
@@ -1,15 +1,9 @@
 from .biosquare import BioSquare
-from .genesis import DicingGod, background_radiate
-from .lensfilter import Dye, Emojify, blockify, digitize
 from .screen import Screen
 
 __all__ = [
-    'BioSquare',
-    'DicingGod',
-    'background_radiate',
-    'Emojify',
-    'Dye',
-    'blockify',
-    'digitize',
-    'Screen',
+    "genesis",
+    "lensfilter",
+    "BioSquare",
+    "Screen",
 ]
