045ed7cff0139264376c2cf6f99b799d599ea00c
Lingxuan Ye <love@lingxuan.io>
2023-11-07 05:27:21 +0800

    Implement `Display` trait and reorganize files

--- rust/src/matrix.rs
+++ rust/src/matrix.rs
@@ -1,4 +1,5 @@
-use std::ops::{Index, IndexMut};
+mod display;
+mod index;
 
 #[derive(PartialEq, Eq)]
 pub struct Matrix<T: Clone + Default> {
@@ -22,25 +23,6 @@ impl<T: Clone + Default> Matrix<T> {
     }
     pub fn shape(&self) -> (usize, usize) {
         self.shape
-    }
-    fn flat_index(&self, index: (usize, usize)) -> usize {
-        assert!(index.0 < self.shape.0, "index 0 out of bounds.");
-        assert!(index.1 < self.shape.1, "index 1 out of bounds.");
-        index.0 * self.shape.1 + index.1
-    }
-}
-
-impl<T: Clone + Default> Index<(usize, usize)> for Matrix<T> {
-    type Output = T;
-    fn index(&self, index: (usize, usize)) -> &Self::Output {
-        &self.data[self.flat_index(index)]
-    }
-}
-
-impl<T: Clone + Default> IndexMut<(usize, usize)> for Matrix<T> {
-    fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
-        let flattened = self.flat_index(index);
-        &mut self.data[flattened]
     }
 }
 
