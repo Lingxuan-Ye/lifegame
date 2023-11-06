d7d632526a3d5c421239ba52f37badc7f8250a80
Lingxuan Ye <love@lingxuan.io>
2023-11-07 03:18:31 +0800

    Add macro `matrix`

--- rust/src/matrix.rs
+++ rust/src/matrix.rs
@@ -1,5 +1,6 @@
 use std::ops::{Index, IndexMut};
 
+#[derive(PartialEq, Eq)]
 pub struct Matrix<T: Clone + Default> {
     data: Vec<T>,
     shape: (usize, usize),
@@ -8,20 +9,23 @@ pub struct Matrix<T: Clone + Default> {
 impl<T: Clone + Default> Matrix<T> {
     pub fn new(nrows: usize, ncols: usize) -> Self {
         let size: usize = nrows * ncols;
-        Matrix {
+        Self {
             data: vec![T::default(); size],
             shape: (nrows, ncols),
         }
     }
+    pub fn from_slice(slice: &[T], shape: (usize, usize)) -> Self {
+        let size = shape.0 * shape.1;
+        let mut data = Vec::from(slice);
+        data.resize(size, T::default());
+        Self { data, shape }
+    }
     pub fn shape(&self) -> (usize, usize) {
         self.shape
     }
-}
-
-impl<T: Clone + Default> Matrix<T> {
     fn flat_index(&self, index: (usize, usize)) -> usize {
-        assert!(index.0 < self.shape.0, "index 0 out of bound.");
-        assert!(index.1 < self.shape.1, "index 1 out of bound.");
+        assert!(index.0 < self.shape.0, "index 0 out of bounds.");
+        assert!(index.1 < self.shape.1, "index 1 out of bounds.");
         index.0 * self.shape.1 + index.1
     }
 }
@@ -38,4 +42,50 @@ impl<T: Clone + Default> IndexMut<(usize, usize)> for Matrix<T> {
         let flattened = self.flat_index(index);
         &mut self.data[flattened]
     }
+}
+
+/// Creates a [`Matrix`] with 2-dimensional array literal.
+///
+///
+/// # Panics
+///
+/// `matrix!` panics if row lengths of the given array are inconsistent.
+///
+/// # Examples
+///
+/// ```
+/// use lifegame::{matrix, matrix::{Matrix}};
+///
+/// matrix![[0, 1, 2, 3, 4], [5, 6, 7, 8, 9],];
+/// let foo: Matrix<i32> = matrix![];
+/// let bar: Matrix<u8> = matrix![[0, 1, 2], [3, 4, 5]];
+/// assert!(bar.shape() == (2, 3));
+/// ```
+#[macro_export]
+macro_rules! matrix {
+    [ $( [ $($x:expr),* $(,)? ] ),* $(,)? ] => {
+        {
+        #[allow(unused_mut)]
+        let mut data = std::vec::Vec::new();
+        #[allow(unused_mut)]
+        let mut shape: (usize, usize) = (0, 0);
+        $(
+            shape.0 += 1;
+            if shape.1 == 0 {
+                $(
+                    shape.1 += 1;
+                    data.push($x);
+                )*
+            } else {
+                #[allow(unused_mut)]
+                let mut count: usize = 0;
+                $(
+                    count += 1;
+                    data.push($x);
+                )*
+                assert!(count == shape.1, "inconsistent row lengths detected.")
+            }
+        )*
+        $crate::matrix::Matrix::from_slice(&data, shape)
+    }};
 }
