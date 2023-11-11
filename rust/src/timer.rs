3bb4536d3dbafe8755dae56e1bce24241ee7399d
Lingxuan Ye <love@lingxuan.io>
2023-11-12 06:34:08 +0800

    Wrap `LimitedDeque<u128>` with `RefCell`

--- rust/src/timer.rs
+++ rust/src/timer.rs
@@ -1,4 +1,5 @@
 use crate::term::{utils::set_dim, TermString};
+use std::cell::RefCell;
 use std::collections::VecDeque;
 use std::time::{Duration, Instant};
 
@@ -35,36 +36,36 @@ impl<T> LimitedDeque<T> {
 
 pub struct Timer {
     timezero: Instant,
-    records: LimitedDeque<u128>,
+    records: RefCell<LimitedDeque<u128>>,
 }
 
 impl Timer {
-    const NANOS_PER_SEC: u128 = Duration::from_secs(1).as_nanos();
-    const NANOS_PER_MILLI: u128 = Duration::from_millis(1).as_nanos();
-    const NANOS_PER_MICRO: u128 = Duration::from_micros(1).as_nanos();
+    pub const NANOS_PER_SEC: u128 = Duration::from_secs(1).as_nanos();
+    pub const NANOS_PER_MILLI: u128 = Duration::from_millis(1).as_nanos();
+    pub const NANOS_PER_MICRO: u128 = Duration::from_micros(1).as_nanos();
 
     const FMT_SEP: &str = " - ";
 
     pub fn new() -> Self {
         Self {
             timezero: Instant::now(),
-            records: LimitedDeque::new(100_000),
+            records: RefCell::new(LimitedDeque::new(100_000)),
         }
     }
 
-    pub fn records(&self) -> &VecDeque<u128> {
-        &self.records.data
+    pub fn records(&self) -> VecDeque<u128> {
+        self.records.borrow().data.clone()
     }
 
-    pub fn check(&mut self, record: bool) -> u128 {
+    pub fn check(&self, record: bool) -> u128 {
         let elapsed = self.timezero.elapsed().as_nanos();
         if record {
-            self.records.push_back(elapsed);
+            self.records.borrow_mut().push_back(elapsed);
         }
         elapsed
     }
 
-    pub fn check_fmt(&mut self, record: bool) -> TermString {
+    pub fn check_fmt(&self, record: bool) -> TermString {
         let mut nanos = self.check(record);
 
         let secs = nanos / Self::NANOS_PER_SEC;
@@ -90,9 +91,9 @@ impl Timer {
             + Self::FMT_SEP
     }
 
-    pub fn check_delta(&mut self, record: bool) -> u128 {
+    pub fn check_delta(&self, record: bool) -> u128 {
         let elapsed = self.check(record);
-        if let Some(&latest) = self.records.back() {
+        if let Some(&latest) = self.records.borrow_mut().back() {
             elapsed - latest
         } else {
             elapsed
@@ -101,7 +102,7 @@ impl Timer {
 
     pub fn reset(&mut self) -> &mut Self {
         self.timezero = Instant::now();
-        self.records.clear();
+        self.records.borrow_mut().clear();
         self
     }
 }
