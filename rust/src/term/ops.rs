15f6feba6688f9db7852abbca00e374ffd18cabb
Lingxuan Ye <love@lingxuan.io>
2023-11-11 21:48:37 +0800

    Implement `Deref` trait for `TermString`

--- rust/src/term/ops.rs
+++ rust/src/term/ops.rs
@@ -1,61 +1,76 @@
-use super::TermString;
-use std::ops::{Add, AddAssign};
+mod add {
+    use super::super::TermString;
+    use std::ops::{Add, AddAssign};
 
-impl AddAssign for TermString {
-    fn add_assign(&mut self, rhs: Self) {
-        self.data += &rhs.data;
-        self.esc_len += rhs.esc_len;
+    impl AddAssign for TermString {
+        fn add_assign(&mut self, rhs: Self) {
+            self.data += &rhs.data;
+            self.esc_len += rhs.esc_len;
+        }
+    }
+
+    impl Add for TermString {
+        type Output = Self;
+        fn add(mut self, rhs: Self) -> Self::Output {
+            self += rhs;
+            self
+        }
+    }
+
+    impl AddAssign<String> for TermString {
+        fn add_assign(&mut self, rhs: String) {
+            self.data += &rhs;
+        }
+    }
+
+    impl Add<String> for TermString {
+        type Output = Self;
+        fn add(mut self, rhs: String) -> Self::Output {
+            self += rhs;
+            self
+        }
+    }
+
+    impl Add<TermString> for String {
+        type Output = TermString;
+        fn add(self, mut rhs: TermString) -> Self::Output {
+            rhs += self;
+            rhs
+        }
+    }
+
+    impl AddAssign<&str> for TermString {
+        fn add_assign(&mut self, rhs: &str) {
+            self.data += rhs;
+        }
+    }
+
+    impl Add<&str> for TermString {
+        type Output = Self;
+        fn add(mut self, rhs: &str) -> Self::Output {
+            self += rhs;
+            self
+        }
+    }
+
+    impl Add<TermString> for &str {
+        type Output = TermString;
+        fn add(self, mut rhs: TermString) -> Self::Output {
+            rhs += self;
+            rhs
+        }
     }
 }
 
-impl Add for TermString {
-    type Output = Self;
-    fn add(mut self, rhs: Self) -> Self::Output {
-        self += rhs;
-        self
-    }
-}
+mod deref {
+    use super::super::TermString;
+    use std::ops::Deref;
 
-impl AddAssign<String> for TermString {
-    fn add_assign(&mut self, rhs: String) {
-        self.data += &rhs;
-    }
-}
+    impl Deref for TermString {
+        type Target = str;
 
-impl Add<String> for TermString {
-    type Output = Self;
-    fn add(mut self, rhs: String) -> Self::Output {
-        self += rhs;
-        self
-    }
-}
-
-impl Add<TermString> for String {
-    type Output = TermString;
-    fn add(self, mut rhs: TermString) -> Self::Output {
-        rhs += self;
-        rhs
-    }
-}
-
-impl AddAssign<&str> for TermString {
-    fn add_assign(&mut self, rhs: &str) {
-        self.data += rhs;
-    }
-}
-
-impl Add<&str> for TermString {
-    type Output = Self;
-    fn add(mut self, rhs: &str) -> Self::Output {
-        self += rhs;
-        self
-    }
-}
-
-impl Add<TermString> for &str {
-    type Output = TermString;
-    fn add(self, mut rhs: TermString) -> Self::Output {
-        rhs += self;
-        rhs
+        fn deref(&self) -> &Self::Target {
+            &self.data
+        }
     }
 }
