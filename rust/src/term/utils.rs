eba50a1fdabe9d50d28151ff7de5c76d90895cfa
Lingxuan Ye <love@lingxuan.io>
2023-11-12 06:31:57 +0800

    Manually dispatch

--- rust/src/term/utils.rs
+++ rust/src/term/utils.rs
@@ -39,3 +39,28 @@ pub fn set_color(value: &str, color: &str) -> TermString {
 pub fn set_color_bg(value: &str, color: &str) -> TermString {
     get_tstr("background", color) + value + get_tstr("reset", "background")
 }
+
+#[inline]
+pub fn set_bold_t(value: TermString) -> TermString {
+    get_tstr("style", "bold") + value + get_tstr("reset", "bold/dim")
+}
+
+#[inline]
+pub fn set_dim_t(value: TermString) -> TermString {
+    get_tstr("style", "dim") + value + get_tstr("reset", "bold/dim")
+}
+
+#[inline]
+pub fn set_bold_dim_t(value: TermString) -> TermString {
+    get_tstr("style", "bold") + get_tstr("style", "dim") + value + get_tstr("reset", "bold/dim")
+}
+
+#[inline]
+pub fn set_color_t(value: TermString, color: &str) -> TermString {
+    get_tstr("foreground", color) + value + get_tstr("reset", "foreground")
+}
+
+#[inline]
+pub fn set_color_bg_t(value: TermString, color: &str) -> TermString {
+    get_tstr("background", color) + value + get_tstr("reset", "background")
+}
