887c34e341f2c624267edbed4f4c91ced4efaa26
Lingxuan Ye <love@lingxuan.io>
2023-11-14 06:08:07 +0800

    Now `DicingGod` accept none value for rng seed

--- rust/src/genesis.rs
+++ rust/src/genesis.rs
@@ -4,12 +4,12 @@ use rand::Rng;
 use rand_chacha::{rand_core::SeedableRng, ChaCha8Rng};
 
 pub struct DicingGod {
-    pub seed: u64,
+    pub seed: Option<u64>,
     pub population_density: f64,
 }
 
 impl DicingGod {
-    pub fn new(seed: u64, population_density: f64) -> Self {
+    pub fn new(seed: Option<u64>, population_density: f64) -> Self {
         Self {
             seed,
             population_density,
@@ -19,10 +19,18 @@ impl DicingGod {
 
 impl WorldCreator for DicingGod {
     fn create(&self, nrows: usize, ncols: usize) -> Generation {
-        let mut rng = ChaCha8Rng::seed_from_u64(self.seed);
+        let mut rng: ChaCha8Rng;
+
+        if let Some(seed) = self.seed {
+            rng = ChaCha8Rng::seed_from_u64(seed);
+        } else {
+            rng = ChaCha8Rng::from_entropy();
+        }
+
         let data: Vec<bool> = (0..nrows * ncols)
             .map(|_| rng.gen_bool(self.population_density))
             .collect();
+
         Matrix::from_slice(&data, (nrows, ncols))
     }
 }
