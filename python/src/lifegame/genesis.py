d68a94d386f212a22225cc0b1f084ef6d19a3e41
Lingxuan Ye <love@lingxuan.io>
2023-11-16 09:46:49 +0800

    Add attribute `initial_density`

--- python/src/lifegame/genesis.py
+++ python/src/lifegame/genesis.py
@@ -4,13 +4,15 @@ from .biosquare import Generation
 
 
 class DicingGod:
-    def __init__(self, seed: int | None = None) -> None:
-        self.__seed = seed
+    def __init__(self, seed: int | None, initial_density: float) -> None:
+        if initial_density < 0:
+            raise ValueError("initial density cannot be negative")
+        if initial_density > 1:
+            raise ValueError("initial density cannot be greater than 1")
+        self.seed = seed
+        self.initial_density = initial_density
 
-    @property
-    def seed(self) -> int | None:
-        return self.__seed
-
-    def __call__(self, nrows: int, ncols: int) -> Generation:
+    def create(self, nrows: int, ncols: int) -> Generation:
         rng = np.random.Generator(np.random.PCG64(self.seed))
-        return rng.integers(0, 2, size=(nrows, ncols), dtype=np.bool_)
+        init_gen = rng.random(size=(nrows, ncols)) >= self.initial_density
+        return init_gen
