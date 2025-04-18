db717065490df46ea372a0c0b0b1f0cad85baf57
Lingxuan Ye <love@lingxuan.io>
2025-04-18 10:50:11 +0800

    refactor: rewrite `Bit`

--- src/filter/bit.rs
+++ src/filter/bit.rs
@@ -3,32 +3,15 @@ use crate::cell::Cell;
 use crossterm::style::{StyledContent, Stylize};
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
-pub struct Bit {
-    dead: StyledContent<char>,
-    alive: StyledContent<char>,
-}
-
-impl Bit {
-    pub fn half_width() -> Self {
-        let dead = '0'.green().bold().dim();
-        let alive = '1'.green().bold();
-        Self { dead, alive }
-    }
-
-    pub fn full_width() -> Self {
-        let dead = '０'.green().bold().dim();
-        let alive = '１'.green().bold();
-        Self { dead, alive }
-    }
-}
+pub struct Bit;
 
 impl Filter for Bit {
-    type View = StyledContent<char>;
+    type View = StyledContent<&'static str>;
 
     fn filter(&self, cell: Cell) -> Self::View {
         match cell {
-            Cell::Dead => self.dead,
-            Cell::Alive => self.alive,
+            Cell::Dead => " 0".green().bold().dim(),
+            Cell::Alive => " 1".green().bold(),
         }
     }
 }
