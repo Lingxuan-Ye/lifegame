66c5778288e4e1216129701202b47645f366cbb1
Lingxuan Ye <love@lingxuan.io>
2025-04-18 17:11:20 +0800

    refactor: remove redundant code

--- src/filter/dye.rs
+++ src/filter/dye.rs
@@ -1,19 +1,8 @@
 use super::Filter;
 use crate::cell::Cell;
 use crossterm::style::{Color, StyledContent, Stylize};
-use rand::seq::IteratorRandom;
 
 const PLACEHOLDER: &str = "  ";
-const CHOICES: [Color; 8] = [
-    Color::Black,
-    Color::Red,
-    Color::Green,
-    Color::Yellow,
-    Color::Blue,
-    Color::Magenta,
-    Color::Cyan,
-    Color::White,
-];
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct Dye {
@@ -22,35 +11,9 @@ pub struct Dye {
 }
 
 impl Dye {
-    pub fn new(dead: Option<Color>, alive: Option<Color>) -> Self {
-        let mut buffer = [Color::Reset; 2];
-
-        CHOICES
-            .into_iter()
-            .choose_multiple_fill(&mut rand::rng(), &mut buffer);
-
-        let (dead, alive) = match (dead, alive) {
-            (None, None) => (buffer[0], buffer[1]),
-            (None, Some(alive)) => {
-                if buffer[0] != alive {
-                    (buffer[0], alive)
-                } else {
-                    (buffer[1], alive)
-                }
-            }
-            (Some(dead), None) => {
-                if buffer[0] != dead {
-                    (dead, buffer[0])
-                } else {
-                    (dead, buffer[1])
-                }
-            }
-            (Some(dead), Some(alive)) => (dead, alive),
-        };
-
+    pub fn new(dead: Color, alive: Color) -> Self {
         let dead = PLACEHOLDER.on(dead);
         let alive = PLACEHOLDER.on(alive);
-
         Self { dead, alive }
     }
 }
